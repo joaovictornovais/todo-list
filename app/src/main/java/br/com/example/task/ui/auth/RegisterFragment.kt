7aa28b2d92234ec1a02e82f0dfe83f7c881498d1
João Victor <joao.dipaay@gmail.com>
2023-02-03 16:06:54 -0300

    Screens and navigation

---
+++ app/src/main/java/br/com/example/task/ui/auth/RegisterFragment.kt
@@ -0,0 +1,18 @@
+package br.com.example.task.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.example.task.R
+
+class RegisterFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+}
